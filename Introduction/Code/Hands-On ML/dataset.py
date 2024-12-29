9ed1f25f0bd776447b83ea0afb593614939bcc67
Dmytro Posyliuzhnyi <dmytro.pos.job@gmail.com>
2024-12-29 22:50:52 +0100

    Added code that splits the dataset into test and training sets

--- Introduction/Code/Hands-On ML/dataset.py
+++ Introduction/Code/Hands-On ML/dataset.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import pandas as pd
 import tarfile
 import urllib.request
+import numpy as np
 
 IMAGES_PATH = Path() / "images" / "end_to_end_project"
 IMAGES_PATH.mkdir(parents=True, exist_ok=True)
@@ -20,7 +22,6 @@ def load_housing_data():
 
 
 housing = load_housing_data()
-print(housing.describe())
 
 
 def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
@@ -42,4 +43,24 @@ def visualize_dataset():
     plt.show()
 
 
-visualize_dataset()
+def create_income_category_column():
+    housing["income_cat"] = pd.cut(housing["median_income"],
+                                   bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                                   labels=['0-1.5', '1.5-3', '3-4.5', '4.5-6', '6-infinity'])
+
+
+def plot_income_category_histogram(dataset):
+    dataset["income_cat"].value_counts().sort_index().plot.bar(rot=0, grid=True)
+    plt.xlabel("Income category")
+    plt.ylabel("Number of districts")
+    save_fig("housing_income_cat_bar_plot")  # extra code
+    plt.show()
+
+
+create_income_category_column()
+
+strat_train_set, strat_test_set = train_test_split(housing, test_size=0.2, stratify=housing["income_cat"],
+                                                   random_state=42)
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
