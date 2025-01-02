cbf542816937a48773f005ee98d668fed786e5e2
Dmytro Posyliuzhnyi <dmytro.pos.job@gmail.com>
2025-01-02 18:49:04 +0100

    Update pipeline_builder.py

--- Introduction/Code/Hands-On ML/pipeline_builder.py
+++ Introduction/Code/Hands-On ML/pipeline_builder.py
@@ -2,10 +2,21 @@ import numpy as np
 from sklearn.pipeline import make_pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, FunctionTransformer
+from custom_transformers import ClusterSimilarity
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer, make_column_selector
 
 
-def build(data):
-    print('a')
+def get_preprocessing_pipeline():
+    preprocessing = ColumnTransformer([
+        ("bedrooms", ratio_pipeline(), ["total_bedrooms", "total_rooms"]),
+        ("rooms_per_house", ratio_pipeline(), ["total_rooms", "households"]),
+        ("people_per_house", ratio_pipeline(), ["population", "households"]),
+        ("log", log_pipeline, ["total_bedrooms", "total_rooms", "population", "households", "median_income"]),
+        ("geo", cluster_simil, ["latitude", "longitude"]),
+        ("cat", cat_pipeline, make_column_selector(dtype_include=object))
+    ], remainder=default_num_pipeline)
+    return preprocessing
 
 
 def column_ratio(x):
@@ -18,7 +29,7 @@ def ratio_name(function_transformer, feature_names_in):
 
 def ratio_pipeline():
     return make_pipeline(
-        SimpleImputer(strategy='Median'),
+        SimpleImputer(strategy='median'),
         FunctionTransformer(column_ratio, feature_names_out=ratio_name),
         StandardScaler())
 
@@ -29,4 +40,7 @@ log_pipeline = make_pipeline(
     StandardScaler()
 )
 
-# TODO: complete the pipeline. Next step is to create custom ClusterSimilarity transformer.
+cat_pipeline = make_pipeline(SimpleImputer(strategy="most_frequent"), OneHotEncoder(handle_unknown="ignore"))
+
+cluster_simil = ClusterSimilarity(n_clusters=10, gamma=1., random_state=42)
+default_num_pipeline = make_pipeline(SimpleImputer(strategy='median'), StandardScaler())
